25598f12688314d5d2b5b87492a1d03eb26fbcb8
faizsarwar <56604615+faizsarwar@users.noreply.github.com>
2025-02-14 02:58:51 +0500

    added helper fns

--- pallets/ceph/src/lib.rs
+++ pallets/ceph/src/lib.rs
@@ -31,9 +31,6 @@ pub mod pallet {
 	}
 
 	#[pallet::storage]
-	pub type Something<T> = StorageValue<_, u32>;
-
-	#[pallet::storage]
 	#[pallet::getter(fn storage_requests)]
 	pub type StorageRequests<T: Config> = StorageDoubleMap<
 		_,
@@ -52,6 +49,16 @@ pub mod pallet {
 		Blake2_128Concat,
 		Vec<u8>,          // Request ID
 		Option<StorageRequestAssignment<T::AccountId, BlockNumberFor<T>>>,
+		ValueQuery
+	>;
+
+	#[pallet::storage]
+	#[pallet::getter(fn user_stored_files)]
+	pub type UserStoredFiles<T: Config> = StorageMap<
+		_,
+		Blake2_128Concat,
+		T::AccountId,    // User ID
+		Vec<Vec<u8>>,    // List of file hashes stored by this user
 		ValueQuery
 	>;
 
@@ -97,6 +104,34 @@ pub mod pallet {
 					Ok(())
 				},
 			}
+		}
+	}
+
+	impl<T: Config> Pallet<T> {
+		/// Add a file hash to the user's stored files list
+		pub fn add_user_stored_file(user: &T::AccountId, file_hash: Vec<u8>) {
+			UserStoredFiles::<T>::mutate(user, |files| {
+				if !files.contains(&file_hash) {
+					files.push(file_hash);
+				}
+			});
+		}
+
+		/// Remove a file hash from the user's stored files list
+		pub fn remove_user_stored_file(user: &T::AccountId, file_hash: &Vec<u8>) {
+			UserStoredFiles::<T>::mutate(user, |files| {
+				files.retain(|hash| hash != file_hash);
+			});
+		}
+
+		/// Get all files stored by a user
+		pub fn get_user_stored_files(user: &T::AccountId) -> Vec<Vec<u8>> {
+			UserStoredFiles::<T>::get(user)
+		}
+
+		/// Check if a specific file is stored by a user
+		pub fn is_file_stored_by_user(user: &T::AccountId, file_hash: &Vec<u8>) -> bool {
+			UserStoredFiles::<T>::get(user).contains(file_hash)
 		}
 	}
 }
