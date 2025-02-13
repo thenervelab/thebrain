c0d6b24e4e84402af65cda81747d90248c5dc2dc
faizsarwar <56604615+faizsarwar@users.noreply.github.com>
2025-02-14 02:56:42 +0500

    added types and storage items to store s3 storage request

--- pallets/ceph/src/lib.rs
+++ pallets/ceph/src/lib.rs
@@ -2,6 +2,9 @@
 
 pub use pallet::*;
 
+pub use types::*;
+pub mod types;
+
 #[cfg(test)]
 mod mock;
 
@@ -16,8 +19,10 @@ pub mod pallet {
 	use super::*;
 	use frame_support::pallet_prelude::*;
 	use frame_system::pallet_prelude::*;
+	use sp_runtime::Vec;
 
 	#[pallet::pallet]
+	#[pallet::without_storage_info]
 	pub struct Pallet<T>(_);
 
 	#[pallet::config]
@@ -27,6 +32,28 @@ pub mod pallet {
 
 	#[pallet::storage]
 	pub type Something<T> = StorageValue<_, u32>;
+
+	#[pallet::storage]
+	#[pallet::getter(fn storage_requests)]
+	pub type StorageRequests<T: Config> = StorageDoubleMap<
+		_,
+		Blake2_128Concat, 
+		T::AccountId,     // User ID
+		Blake2_128Concat, 
+		Vec<u8>,          // File Hash
+		Option<StorageRequest<T::AccountId, BlockNumberFor<T>>>,
+		ValueQuery
+	>;
+
+	#[pallet::storage]
+	#[pallet::getter(fn storage_request_assignments)]
+	pub type StorageRequestAssignments<T: Config> = StorageMap<
+		_,
+		Blake2_128Concat,
+		Vec<u8>,          // Request ID
+		Option<StorageRequestAssignment<T::AccountId, BlockNumberFor<T>>>,
+		ValueQuery
+	>;
 
 	#[pallet::event]
 	#[pallet::generate_deposit(pub(super) fn deposit_event)]
---
+++ pallets/ceph/src/types.rs
@@ -0,0 +1,67 @@
+use frame_support::pallet_prelude::*;
+use frame_system::pallet_prelude::*;
+use scale_info::TypeInfo;
+use sp_runtime::Vec;
+
+/// Represents a storage request made by a user
+#[derive(Encode, Decode, Clone, PartialEq, Eq, Default, TypeInfo)]
+pub struct StorageRequest<AccountId, BlockNumber> {
+    /// Unique identifier for the file/request
+    pub file_hash: Vec<u8>,
+    
+    /// Name of the file being stored
+    pub file_name: Vec<u8>,
+    
+    /// Size of the file in bytes
+    pub file_size: u64,
+    
+    /// User who initiated the storage request
+    pub user_id: AccountId,
+    
+    /// Indicates if the request has been assigned to miners
+    pub is_assigned: bool,
+    
+    /// Block number when the request was created
+    pub created_at: BlockNumber,
+    
+    /// Number of replicas requested
+    pub requested_replicas: u32,
+    
+    /// Optional metadata or additional file information
+    pub metadata: Option<Vec<u8>>,
+}
+
+/// Represents the assignment of a storage request to miners
+#[derive(Encode, Decode, Clone, PartialEq, Eq, Default, TypeInfo)]
+pub struct StorageRequestAssignment<AccountId, BlockNumber> {
+    /// Unique identifier for the storage request
+    pub request_id: Vec<u8>,
+    
+    /// File hash associated with the request
+    pub file_hash: Vec<u8>,
+    
+    /// Miner ID assigned to store the file
+    pub miner_id: Vec<u8>,
+    
+    /// User who initiated the original storage request
+    pub user_id: AccountId,
+    
+    /// URL or location where the file will be stored
+    pub file_url: Vec<u8>,
+    
+    /// Indicates if the assignment has been fulfilled
+    pub is_fulfilled: bool,
+    
+    /// Block number when the assignment was created
+    pub created_at: BlockNumber,
+    
+    /// Block number when the assignment was fulfilled
+    pub fulfilled_at: Option<BlockNumber>,
+    
+    /// S3 specific storage metadata
+    pub s3_bucket: Option<Vec<u8>>,
+    pub s3_key: Option<Vec<u8>>,
+    
+    /// Optional additional storage parameters
+    pub storage_params: Option<Vec<u8>>,
+}
