4eaa766a9ec9d9a30cc59667f5c03af781ef45cd
Francisco Silva <franjs.francisco@gmail.com>
2025-10-22 16:59:38 +0200

    remove unnecessary feature flag from migrate_to_direct_staker

--- pallets/staking/src/pallet/impls.rs
+++ pallets/staking/src/pallet/impls.rs
@@ -1953,7 +1953,6 @@ impl<T: Config> sp_staking::StakingUnchecked for Pallet<T> {
 		Ok(())
 	}
 
-	#[cfg(feature = "runtime-benchmarks")]
 	fn migrate_to_direct_staker(who: &Self::AccountId) {
 		assert!(VirtualStakers::<T>::contains_key(who));
 		let ledger = StakingLedger::<T>::get(Stash(who.clone())).unwrap();
