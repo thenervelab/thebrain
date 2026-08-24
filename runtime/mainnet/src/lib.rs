1861dd17830e77109e5feb625ab83acf1ce63ccd
faizsarwar <faizsarwar44@gmail.com>
2026-08-24 20:57:22 +0500

    updated spec_version

--- runtime/mainnet/src/lib.rs
+++ runtime/mainnet/src/lib.rs
@@ -592,7 +592,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("hippius"),
 	impl_name: create_runtime_str!("hippius"),
 	authoring_version: 1,
-	spec_version: 92008,
+	spec_version: 92009,
 	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
